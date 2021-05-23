0070875a86a3ee4c863eac6053f3793dbc235c1b
mmohiesen96 <mmohiesen996@gmail.com>
2021-05-23 17:40:16 +0300

    lab06 finished

--- README.md
+++ README.md
@@ -1,16 +1,17 @@
-# Project Name
+# City Explorer
 
-**Author**: Your Name Goes Here
-**Version**: 1.0.0 (increment the patch/fix version number if you make more commits past your first submission)
+**Author**: Mohammed Mohiesen
+**Version**: 1.0.1 
 
 ## Overview
-<!-- Provide a high level overview of what this application is and why you are building it, beyond the fact that it's an assignment for this class. (i.e. What's your problem domain?) -->
+City Explorer will help Guide you through cities using maps an IQ API .
 
 ## Getting Started
-<!-- What are the steps that a user must take in order to build this app on their own machine and get it running? -->
+- Enter the name of the city You want to explore .
+- Hit Explore !! .
 
 ## Architecture
-<!-- Provide a detailed description of the application design. What technologies (languages, libraries, etc) you're using, and any other relevant design information. -->
+- A react application 
 
 ## Change Log
 <!-- Use this area to document the iterative changes made to your application as each feature is successfully implemented. Use time stamps. Here's an example:
@@ -21,12 +22,52 @@
 <!-- Give credit (and a link) to other people or resources that helped you build this application. -->
 
 
-Name of feature: ________________________________
+Name of feature: Step 1
 
-Estimate of time needed to complete: _____
+Estimate of time needed to complete: 45 Minutes
 
-Start time: _____
+Start time: 3:00
 
-Finish time: _____
+Finish time: 3:40
 
-Actual time needed to complete: _____
+Actual time needed to complete: 40 minutes
+
+
+
+Name of feature: Step 2
+
+Estimate of time needed to complete: 20 Minutes
+
+Start time: 3:40
+
+Finish time: 4:00
+
+Actual time needed to complete: 30 minutes
+
+
+
+
+
+Name of feature: Step 3
+
+Estimate of time needed to complete: 45 Minutes
+
+Start time: 4:00
+
+Finish time: 4:30
+
+Actual time needed to complete: 30 minutes
+
+
+
+
+
+Name of feature: Step 4
+
+Estimate of time needed to complete: 45 Minutes
+
+Start time: 4:30
+
+Finish time: 4:35
+
+Actual time needed to complete: 5 minutes
--- src/App.js
+++ src/App.js
@@ -1,25 +1,86 @@
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import React from 'react';
+import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { Form, Button, Card , Alert} from 'react-bootstrap/';
+import axios from 'axios';
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      resultQuery: '',
+      foundQuery: '',
+      showAlert: false,
+      displayCard: false,
+      showMap: false
+    }
+  }
+
+  getQuery = async (event) => {
+    event.preventDefault();
+    let locationLink = `https://eu1.locationiq.com/v1/search.php?key=pk.80438a552b9686e0e4dace4a068a30eb&q=${this.state.resultQuery}&format=json`;
+    try {
+      let city = await axios.get(locationLink);
+      this.setState({
+        foundQuery: city.data[0],
+        displayCard: true,
+        showMap: true,
+        showAlert: false
+      })
+    }
+
+    catch {
+      this.setState({
+        showAlert: true,
+        showMap: false
+      })
+    }
+  }
+
+  updateResultQuery = event => {
+    this.setState({
+      resultQuery: event.target.value
+    })
+    console.log(this.state.resultQuery);
+  }
+  render() {
+    return (
+      <>
+              {
+          this.state.showAlert &&
+        <Alert variant='danger'>
+          Enter a proper City name Please !!
+        </Alert>
+        }
+        <Form onSubmit={this.getQuery}>
+          <Form.Group controlId="formBasicEmail">
+            <Form.Label>City Name</Form.Label>
+            <Form.Control type="text" placeholder="Enter City" onChange={this.updateResultQuery} />
+            <Form.Text className="text-muted">
+              Enter The City You want to Explore
+    </Form.Text>
+          </Form.Group>
+          <Button variant="primary" type="submit">
+            Explore!
+  </Button>
+        </Form>{
+          this.state.displayCard &&
+          <Card style={{ width: '18rem' }}>
+            <Card.Img variant="top" src={`https://maps.locationiq.com/v3/staticmap?key=pk.80438a552b9686e0e4dace4a068a30eb&q=&center=${this.state.foundQuery.lat},${this.state.foundQuery.lon}`} />
+            <Card.Body>
+              <Card.Title>{this.state.foundQuery.display_name}</Card.Title>
+              <Card.Text>
+                {this.state.foundQuery.lat}<br></br>
+                {this.state.foundQuery.lon}
+              </Card.Text>
+            </Card.Body>
+          </Card>
+        }
+      </>
+    )
+
+  }
 }
 
 export default App;
